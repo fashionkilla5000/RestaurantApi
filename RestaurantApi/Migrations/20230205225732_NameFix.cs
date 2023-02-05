80d36db8aee1a1e6f04da71ccd8e77009de57401
pio1755 <pio1755@gmail.com>
2023-02-06 00:00:08 +0100

    newSend

---
+++ RestaurantApi/Migrations/20230205225732_NameFix.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantApi.Migrations
+{
+    public partial class NameFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Restaurants",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 25);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Restaurants",
+                type: "int",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+    }
+}
--- RestaurantApi/RestaurantSeeder.cs
+++ RestaurantApi/RestaurantSeeder.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using RestaurantApi.Entities;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using RestaurantApi.Entities;
 
 namespace RestaurantApi
 {
@@ -13,25 +14,85 @@ namespace RestaurantApi
         {
             _dbContext = dbContext;
         }
+
         public void Seed()
         {
             if (!_dbContext.Database.CanConnect())
             {
-                if(!_dbContext.Restaurants.Any())
+                if (!_dbContext.Restaurants.Any())
                 {
                     var restaurants = GetRestaurants();
                     _dbContext.Restaurants.AddRange(restaurants);
                 }
+
             }
+
         }
+
         private IEnumerable<Restaurant> GetRestaurants()
         {
             var restaurants = new List<Restaurant>()
             {
-                Name = 'KFC';
-            `   
-            }
-            return restaurant;
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description = "xXD XDDF SDFDSBFgb sfvwebacieac",
+                    ContactEmail="con@kfc.pl",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "New one",
+                            Price = 10.3M,
+                        },
+                        new Dish()
+                        {
+                            Name ="two",
+                            Price = 5.7M,
+                        },
+
+                    },
+                    Adress = new Adress()
+                    {
+                        City= "Warsaw",
+                        Street = "Warszawska 1",
+                        PostalCode= "01-321",
+                    }
+                },
+
+                 new Restaurant()
+                {
+                    Name = "McDonald",
+                    Category = "Fast Food",
+                    Description = "xX22D X3DDF nomSDFDSBFgb sfvwebacieac",
+                    ContactEmail="con@mcd.pl",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "pierww",
+                            Price = 7.3M,
+                        },
+                        new Dish()
+                        {
+                            Name ="drugie",
+                            Price = 7.7M,
+                        },
+
+                    },
+                    Adress = new Adress()
+                    {
+                        City= "Warsaw",
+                        Street = "Warszawska 2",
+                        PostalCode= "01-321",
+                    }
+                },
+
+            };
+            return restaurants;
         }
     }
 }
